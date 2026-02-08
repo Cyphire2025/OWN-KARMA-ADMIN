24b6e497a49e884ba9d41aab0c1850ace68c51c7
Cyphire2025 <cyphire1@gmail.com>
2026-02-08 17:55:34 +0530

    chore: push production admin build

---
+++ .medusa/server/public/admin/assets/chunk-F6ZOHZVB-49BP7dUD.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-GW3g8wxz.js";var e=({children:s})=>r.jsx("span",{className:"sr-only",children:s});export{e as V};
---
+++ .medusa/server/public/admin/assets/home-KFQGXKRT-CYar0iSl.js
@@ -0,0 +1,1 @@
+import{u as r,r as t,j as s}from"./index-GW3g8wxz.js";var o=()=>{const e=r();return t.useEffect(()=>{e("/orders",{replace:!0})},[e]),s.jsx("div",{})};export{o as Component};
